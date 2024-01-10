b07a7c6f11d5a93872036b6dd428380fd898b417
VladiDemirev <v.demirev@yahoo.com>
2024-01-10 18:33:27 +0200

    Create a function called kwargs_length() that can receive some keyword arguments and return their length.

---
+++ functions_advanced_exercise/02_keyword_arguments_length.py
@@ -0,0 +1,9 @@
+def kwargs_length(**kwargs):
+    return len(kwargs.items())
+
+
+dictionary = {'name': 'Peter', 'age': 25}
+print(kwargs_length(**dictionary))
+
+dictionary = {}
+print(kwargs_length(**dictionary))
