1008ac137f76614b3f77ff4be2c0390741fdbfa9
VladiDemirev <v.demirev@yahoo.com>
2024-02-02 18:26:11 +0200

    Create a function called start_playing which will receive an instance and will return its play() method.

---
+++ OOP/polymorphism_and_abstraction_lab/03_playing.py
@@ -0,0 +1,22 @@
+def start_playing(class_instance):
+    return class_instance.play()
+
+
+#   TEST CODE
+
+class Guitar:
+    def play(self):
+        return "Playing the guitar"
+
+
+guitar = Guitar()
+print(start_playing(guitar))
+
+
+class Children:
+    def play(self):
+        return "Children are playing"
+
+
+children = Children()
+print(start_playing(children))
