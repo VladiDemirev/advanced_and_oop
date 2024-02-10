b3843d1847e22d7d3146bd7f64d3455c2dfd94f5
VladiDemirev <v.demirev@yahoo.com>
2024-02-10 10:52:44 +0200

    Create a generator function called read_next() which should receive a different number of arguments (all iterable). On each iteration, the function should return each element from each sequence.

---
+++ OOP/iterators_and_generators_exercise/07_reader.py
@@ -0,0 +1,16 @@
+def read_next(*args):
+    for el in args:
+        for char in el:
+            yield char
+
+
+
+# TEST CODE
+
+for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
+    print(item, end='')
+
+print()
+
+for i in read_next("Need", (2, 3), ["words", "."]):
+    print(i)
