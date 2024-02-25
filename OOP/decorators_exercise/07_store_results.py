7561acdbf92ed4704d9219d9aaec8343a732622f
VladiDemirev <v.demirev@yahoo.com>
2024-02-25 18:54:21 +0200

    Create a class called store_results. It should be used as a decorator and store information about the executed functions in a file called results.txt in the format: "Function {func_name} was called. Result: {func_result}"

---
+++ OOP/decorators_exercise/07_store_results.py
@@ -0,0 +1,24 @@
+class store_results:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        with open("results.txt", "a") as file:
+            file.write(f"Function '{self.func.__name__}' was called. " \
+                       f"Result: {self.func(*args, *kwargs)}\n")
+
+
+#  Test Code
+
+@store_results
+def add(a, b):
+    return a + b
+
+
+@store_results
+def mult(a, b):
+    return a * b
+
+
+add(2, 2)
+mult(6, 4)
